b879d60d1757e1b5eb08a035acc0660dbc281377
Pete Fein <pete@snake.dev>
2024-07-12 17:24:53 -0400

    work with a new duckdb version

--- nyc_transit/macros/utils.sql
+++ nyc_transit/macros/utils.sql
@@ -2,10 +2,10 @@
 -- convert to BOOL using the specified values
 -- for unknown values, raises an error
 (case
-    when {{column_name}} = '{{true_value}}' then true
+    when {{column_name}} = {{true_value}} then true
     when {{column_name}} = '{{false_value}}' then false
     when {{column_name}} = '{{null_value}}' then null
     when {{column_name}} is null then null
-    else {{column_name}}
+    else null -- treat any other value as null
 end)::bool
 {%- endmacro %}
